48c6fa6e0ab23c4e5dce463e90bcaecee6408188
Mohamed Amine Khallouk <mkhallou@e1r4p7.1337.ma>
2024-11-26 15:41:31 +0100

    ft_printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: mkhallou <mkhallou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/25 19:30:48 by mkhallou          #+#    #+#             */
-/*   Updated: 2024/11/25 22:06:18 by mkhallou         ###   ########.fr       */
+/*   Updated: 2024/11/26 15:41:13 by mkhallou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,13 +49,4 @@ int	ft_printf(const char *format, ...)
 	}
 	va_end(args);
 	return (cp);
-}
-
-int main()
-{
-	int a = ft_printf("b-h%x T}%x%dB%p%i,IvRs%XmZj%cUU`MOBVisJ%s\r04;@p%X%sX", -1676595710, 1453192932, -96482680, (void *)-2172162475412057634, 536580747, -906831861, -837596881, "X(WRs)rp>=<C*T?WB'sZchM-w<\\bu\"\fE8X!H[@FsB*$\n`<4p*uaz(}aE7w\\q-0BI ZbJxWp;\vWJ : 2z8hhbrN0}2ST4", -11311222, "mhD6|fc3c\vL`T@1-YoDf%Q%GWO&T4K9'(uKe\v]ulEC\r[\nRb%8-3^KQu=[K!/");
-	puts("");
-	int b = printf("b-h%x T}%x%dB%p%i,IvRs%XmZj%cUU`MOBVisJ%s\r04;@p%X%sX", -1676595710, 1453192932, -96482680, (void *)-2172162475412057634, 536580747, -906831861, -837596881, "X(WRs)rp>=<C*T?WB'sZchM-w<\\bu\"\fE8X!H[@FsB*$\n`<4p*uaz(}aE7w\\q-0BI ZbJxWp;\vWJ : 2z8hhbrN0}2ST4", -11311222, "mhD6|fc3c\vL`T@1-YoDf%Q%GWO&T4K9'(uKe\v]ulEC\r[\nRb%8-3^KQu=[K!/");
-	printf("\n%d\n%d", a, b);
-	// printf("\n%d",b);
 }
